6a10a736039c9f66439cb2a3b7cb94c23d4a7e75
zhangke <changhe626@163.com>
2018-09-19 19:58:08 +0800

    添加了netty的代码 2018年9月19日19:58:02

---
+++ netty/src/main/java/com/onyx/netty/Start.java
@@ -0,0 +1,21 @@
+package com.onyx.netty;
+
+import com.onyx.netty.pool.NioSelectorRunnablePool;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 启动主函数
+ */
+public class Start {
+
+	public static void main(String[] args) {
+		//初始化线程
+		NioSelectorRunnablePool nioSelectorRunnablePool = new NioSelectorRunnablePool(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+		//获取服务类
+		ServerBootstrap bootstrap = new ServerBootstrap(nioSelectorRunnablePool);
+		//绑定端口
+		bootstrap.bind(10010);
+		System.out.println("start");
+	}
+}
